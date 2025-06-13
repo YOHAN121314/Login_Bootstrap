6e2a9b79e7f9f6482785b8895aaf018342cd4423
Yohan Andrey Rivera Santiago <acuribel@ufpso.edu.co>
2025-06-13 11:03:45 -0500

    recuperar_contraseña

---
+++ js/aprender.js
@@ -0,0 +1,51 @@
+// ########FORMAS DE IMPRIMIR JS
+console.log ("HOLA PROFE")
+    // alert("hola profe")
+    // document.body.innerHTML = "<h1>hola querido profe</h1>"
+    // document.body.innerText = "<h1>hola querido profe</h1>"
+
+
+
+// ######## VARIABLES EN JS => let, var, const
+const edad = 18; // variable que no puede ser alterada
+let edad_dos = 18; // variable para bloques
+var nombre = "yohan rivera" // variable global
+
+
+// ######## TIPOS DE DATOS enteros, decimal,string, booleanos, arrays
+var entero = 20;
+var decimal = 20.5;
+var string = "mi nombre es: yohan"
+var boolean = false // false
+var array_string = ["lun","mar","mie","jue","vie","sab","dom"];
+var array_numerico = [1,2,3,4,5];
+var array_mixto = [1,2,3,4,5, "mar","mie","jue", false,true];
+
+
+
+// ######## tipo de variables  pascalcasa, camelcase, snake_casa
+
+
+// ######## OPERADORES ARITMETICOS => -, +, /, *, %
+var suma = entero + decimal;
+var resta = decimal - entero;
+var multi = decimal * entero;
+var divi = decimal / entero; 
+var modulo = decimal % 2;
+
+// ######## operadores de comparacion
+
+// ######## ESTRUCTURAS DE CONTROL => de repeticion (bucles), de condicion (ales)
+// inicializador - comparacion - contador
+/*console.log(array_string)
+console.log(array_string.length)
+var impresion = "";
+for (let i = 0; i < array_string.length; i++) {
+    console.log(array_string[i]) 
+    impresion = impresion + array_string[i]
+}
+Swal.fire(impresion);*/
+
+
+// PARA IMPRIMIR 
+console.log(modulo)
